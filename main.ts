5ebce2b7fc704f8f3134953e80a8aabbf90d669f
B207 <91238174+csjh240@users.noreply.github.com>
2021-10-16 09:03:13 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
+let score = 0
+// 記分員 micro:bitB
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 1)
     basic.pause(500)
-    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
 })
+/**
+ * A GND <---------------> B  GND
+ * 
+ * A P0     <---------------> B  P1
+ * 
+ * A P2 ------------LED-----------A GND
+ */
+// 記分板micro:bitA
 basic.forever(function () {
-	
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        score += 1
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.showNumber(score)
+        basic.pause(1000)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
 })
